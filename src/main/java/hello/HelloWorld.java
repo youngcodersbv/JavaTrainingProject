7acaf4803f02f693d479631c5ac02393855e8de7
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-09-22 19:41:23 +0200

    Second commit set up simple maven project

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Outputting to the console
+        System.out.println("HelloWorld");
+    }
+}
